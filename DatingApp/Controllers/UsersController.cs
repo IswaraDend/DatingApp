8c82a81e1e661d176c880de0d067af5ada0ad06e
Iswara Dendy A <dendyartaa@gmail.com>
2023-08-25 14:14:22 +0700

    Add project files.

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // /api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUser()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")] // /api/users/2
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var users = _dataContext.Users.Find(id);
+
+
+            return users;
+            // bisa juga pake return _dataContext.Users.Find(id);
+        }
+
+    }
+
+}
